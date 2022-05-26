f8702743bd8b4d5c80c178ad2e410f43cdf8662a
Hamza-mej <mejdoubi.hamzaa@gmail.com>
2022-05-26 21:13:17 +0000

    Test GetCollection, GetItem, Create, Update and Delete book

--- tests/Functional/BookListingResourceTest.php
+++ tests/Functional/BookListingResourceTest.php
@@ -3,116 +3,120 @@
 namespace App\Tests\Functional;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Book;
 
 class BookListingResourceTest extends ApiTestCase
 {
+    protected function setUp(): void
+    {
+        $this->client = $this->createClient();
+        $this->entityManager = $this->client->getContainer()->get('doctrine')->getManager();
 
+        $this->entityManager->createQuery('DELETE FROM App\Entity\Book')->execute();
+        $this->entityManager->flush();
 
-    // public function testCreateBook(){
+        $book1 = new Book();
+        $book1->setTitle('title_test');
+        $book1->setDescription('description_test');
+        $book1->setPublicationDate(new \DateTime('2022-04-24'));
+        $book1->setGenre('genre_test');
+        $book1->setAuthor(NULL);
 
-    //     $response = static::createClient()->request('POST', '/api/books', [
-    //             'headers' => ['Content-Type' => 'application/json'],
-    //             'json' => [
-    //                 'title' => 'title_test',
-    //                 'description' => 'description_test',
-    //                 'publicationDate' => '2022-04-23',
-    //                 'genre' => 'genre_test',
-    //                 'author' => NULL,
-    //             ],
-    //         ]);
+        $book2 = new Book();
+        $book2->setTitle('title_test2');
+        $book2->setDescription('description_test2');
+        $book2->setPublicationDate(new \DateTime('2022-04-24'));
+        $book2->setGenre('genre_test2');
+        $book2->setAuthor(NULL);
 
-    //     $this->assertResponseStatusCodeSame(201);
+        $this->entityManager->persist($book1);
+        $this->entityManager->persist($book2);
+        $this->entityManager->flush();
 
+        $this->id1 = $book1->getId();
+        $this->id2 = $book2->getId();
+    }
 
-    //     $this->assertJsonContains([
-    //         'title' => 'title_test',
-    //         'description' => 'description_test',
-    //         'publicationDate' => '2022-04-23T00:00:00+02:00',
-    //         'genre' => 'genre_test',
+    public function testGetAllBooks()
+    {
+        $response = $this->client->request('GET', '/api/books', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
 
-    //    ]);
+        $this->assertResponseIsSuccessful();
 
-    // }
+        $this->assertJsonContains([
+            "@context" => "/api/contexts/Book",
+            "@id" => "/api/books",
+            "@type" => "hydra:Collection",
+            "hydra:totalItems" => 2,
+        ]);
 
-    // public function testUpdateBook(){
+        $this->assertCount(2, $response->toArray()['hydra:member']);
+    }
 
-    //     $client = static::createClient();
+    public function testGetBook()
+    {
 
-    //     $client->request('PUT', '/api/books', [
-    //             'headers' => ['Content-Type' => 'application/json'],
-    //             'json' => [
-    //                 'description' => 'Update description',
-    //             ],
-    //         ]);
+        $this->client->request('GET', "/api/books/{$this->id1}", [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
 
-    //     $this->assertResponseIsSuccessful();
+        $this->assertResponseIsSuccessful();
 
+        $this->assertJsonContains([
+            "@id" => "/api/books/{$this->id1}",
+        ]);
+    }
 
-    //     $this->assertJsonContains([
-    //         '@id' => '/api/books',
-    //         'description' => 'Update description',
-    //    ]);
+    public function testCreateBook()
+    {
+        $this->client->request('POST', '/api/books', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'title' => 'title_test3',
+                'description' => 'description_test3',
+                'publicationDate' => '2022-04-25',
+                'genre' => 'genre_test3',
+                'author' => NULL,
+            ],
+        ]);
 
-    // }
+        $this->assertResponseStatusCodeSame(201);
 
-    // public function testDeleteBook(){
+        $this->assertJsonContains([
+            'title' => 'title_test3',
+            'description' => 'description_test3',
+            'genre' => 'genre_test3',
+        ]);
+    }
 
-    //     $client = static::createClient();
+    public function testUpdateBook()
+    {
+        $this->client->request('PUT', "/api/books/{$this->id1}", [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'description' => 'Update description',
+            ],
+        ]);
 
-    //     $client->request('DELETE', '/api/books/1', [
-    //             'headers' => ['Content-Type' => 'application/json'],
-    //             'json' => [
-    //             ],
-    //         ]);
+        $this->assertResponseIsSuccessful();
 
-    //     $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            "@id" => "/api/books/{$this->id1}",
+            'description' => 'Update description',
+        ]);
+    }
 
+    public function testDeleteBook()
+    {
+        $this->client->request('DELETE',  "/api/books/{$this->id1}", [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
 
-    // //     $this->assertJsonContains([
-    // //         '@id' => '/api/books/1',
-    // //    ]);
-
-    // }
-
-
-
-
-    /***********************************/
-
-
-    // public function testGetCollection(){
-
-    //     $response = static::createClient()->request('GET', '/api/books', [
-    //             'headers' => ['Content-Type' => 'application/json'],
-    //             'json' => [],
-    //         ]);
-
-    //     $this->assertResponseIsSuccessful();
-
-    //     $this->assertJsonContains([
-    //             "@context"=> "/api/contexts/Book",
-    //             "@id"=> "/api/books",
-    //             "@type"=> "hydra:Collection",
-    //             "hydra:totalItems"=> 1,
-    //            ]);
-
-    //     $this->assertCount(1, $response->toArray()['hydra:member']);
-
-    // }
-
-    // public function testGetItem(){
-
-    //     $response = static::createClient()->request('GET', '/api/books/49', [
-    //             'headers' => ['Content-Type' => 'application/json'],
-    //             'json' => [],
-    //         ]);
-
-    //     $this->assertResponseIsSuccessful();
-
-    //     $this->assertJsonContains([
-    //             "@id"=> "/api/books/49",
-    //            ]);
-
-    // }
-
+        $this->assertResponseIsSuccessful();
+    }
 }
